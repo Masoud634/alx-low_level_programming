67eab996a71d3f53b5ce333468d15095647ba8fa
Masoud634 <masoudhusainbakhet@gmail.com>
2022-12-29 06:19:49 -0800

    task2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,24 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 /**
  * main - Entry point
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-		int d;
-
-		for (d = 0; d < 100; d++)
+		char c;
+			
+		for (c='a' ; c<='z' ; c++)
 		{
-			putchar((d / 10) + '0');
-			putchar((d % 10) + '0');
-				if (d != 99)
-					{																			putchar(',');
-																								putchar(' ');
-																							}
+			putchar(c);
 		}
-		putchar('\n');
-
+		
+		putchar('\n')
+		
 		return (0);
 }
